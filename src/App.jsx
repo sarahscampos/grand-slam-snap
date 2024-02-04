8c6e77e518d1d99a0139490cb2150f4b6ff83589
sarah-campos <sarahstephanycampos@gmail.com>
2024-02-04 01:01:01 -0300

    estruturando a galeria[F

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,53 @@ import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/Banner";
 import bannerBackground from '/imagens/banner.png'
+import Galeria from "./componentes/Galeria";
 // na interpolação de string é onde vai o código css
 const FundoGradiente = styled.div`
-  background-color: #000000;
+  background-color: #0a0a0a;
   width: 100%;
   min-height: 100vh;
 `;
+
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto; //A width está definida quando a tela for maior que 1440px, irá aplicar a margem E/D
+  max-width: 100%;
+
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 
 function App() {
 
   return (
     <FundoGradiente>
       <EstilosGlobais />
-      <Cabecalho />
-      <BarraLateral />
-      <Banner 
-        texto='A galeria mais
-        completa de 
-        fotos dos Grand Slams
-        femininos!'
-        backgroundImage={bannerBackground}
-      />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner
+              texto='A galeria mais
+              completa de
+              fotos dos Grand Slams
+              femininos!'
+              backgroundImage={bannerBackground}
+            />
+            <Galeria />
+          </ConteudoGaleria>
+        </MainContainer>
+      </AppContainer>
 
     </FundoGradiente>
   )
--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -9,13 +9,13 @@ const FigureEstilizada = styled.figure`
     min-height: 328px;
     margin: 0;
     border-radius: 20px;
-    max-width: 1156px;
+    max-width: 100%;
     background-size: cover;
 `
 
 const TituloEstilizado = styled.h1`
     font-weight: 400;
-    font-size: 40px;
+    font-size: 36px;
     line-height: 48px;
     color: #FFFFFF;
     max-width: 300px;
